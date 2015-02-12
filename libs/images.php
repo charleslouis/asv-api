cf71d9960a5ded9679f4ee26271dbe8b016420f6
charles-louis <contactokaou@gmail.com>
2015-02-12 13:48:21 +0100

    adds a rectangle image

--- libs/images.php
+++ libs/images.php
@@ -1,3 +1,4 @@
 <?php 
 
-add_image_size( 'squared-image-med', 500, 500, array( 'left', 'top' ) ); // Hard crop left top
+add_image_size( 'squared-image-med', 500, 500, true ); // Hard crop left top
+add_image_size( 'rectangle-image-med', 600, 800, true ); // Hard crop left top
