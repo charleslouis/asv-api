0024832677349869184bf64e23f8e80438e7c6fc
charles-louis <contactokaou@gmail.com>
2015-05-22 12:02:13 +0200

    get a multi user twitter feed

--- twitter-feed-twitteroauth.php
+++ twitter-feed-twitteroauth.php
@@ -4,18 +4,26 @@ Template Name: Twitter Feed oAuth
 */
 ?>
 <?php
+	header('Cache-Control: no-cache, must-revalidate');
+	header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+	header('Content-type: application/json');
+
 	session_start();
 	// require_once("libs/twitteroauth/autoload.php"); //Path to twitteroauth library
 	require "libs/twitteroauth/autoload.php";
 	use Abraham\TwitterOAuth\TwitterOAuth;
 
 	$notweets = get_field('number_of_tweets');
+
+	$twitterhandle = get_field('twitter_handle');
+	
 	$consumerkey = get_field('consumer_key');
 	$consumersecret = get_field('consumer_secret');
 	$accesstoken = get_field('oauth_access_token');
 	$accesstokensecret = get_field('oauth_access_token_secret');
 
-	$twitteruser = get_field('twitter_handle');
+
+
 
 	function getConnectionWithAccessToken($cons_key, $cons_secret, $oauth_token, $oauth_token_secret) {
 	  $connection = new TwitterOAuth($cons_key, $cons_secret, $oauth_token, $oauth_token_secret); 
@@ -24,7 +32,15 @@ Template Name: Twitter Feed oAuth
 	 
 	$connection = getConnectionWithAccessToken($consumerkey, $consumersecret, $accesstoken, $accesstokensecret);
 
-	$statuses = $connection->get("statuses/user_timeline", array("screen_name" => ["Minh_Q_Tran"], "count" => $notweets, "exclude_replies" => true));
 
-	echo json_encode($statuses);
+	if (have_rows('twitter_accounts')) {		
+	    while ( have_rows('twitter_accounts') ) : the_row();
+	        $twitter_account = get_sub_field('twitter_username');
+	        $params =  array("screen_name" => $twitter_account, "count" => $notweets, "exclude_replies" => true, );
+			$twitter_account_timeline = $connection->get("statuses/user_timeline", $params);
+			$news_feed = array_merge((array) $news_feed, $twitter_account_timeline);
+	    endwhile;
+	}
+	// $news_feed[] = $news_feed;
+	echo json_encode($news_feed);
 ?>
