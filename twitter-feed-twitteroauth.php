45b11b47bfd38475e0e2ff243a91315379d66638
charles-louis <contactokaou@gmail.com>
2015-05-26 11:46:23 +0200

    create a text_html version of the tweet

--- twitter-feed-twitteroauth.php
+++ twitter-feed-twitteroauth.php
@@ -38,9 +38,47 @@ Template Name: Twitter Feed oAuth
 	        $twitter_account = get_sub_field('twitter_username');
 	        $params =  array("screen_name" => $twitter_account, "count" => $notweets, "exclude_replies" => true, );
 			$twitter_account_timeline = $connection->get("statuses/user_timeline", $params);
-			$news_feed = array_merge((array) $news_feed, $twitter_account_timeline);
+			$tweet_feed = array_merge((array) $tweet_feed, $twitter_account_timeline);
 	    endwhile;
 	}
-	// $news_feed[] = $news_feed;
-	echo json_encode($news_feed);
+
+
+	$reg_exUrl = "/(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/";
+	$reg_exHash = "/#([a-z_0-9]+)/i";
+	$reg_exUser = "/@([a-z_0-9]+)/i";
+
+	foreach ($tweet_feed as $tweet) {
+	    
+	    $tweet_text = $tweet->text; //get the tweet
+	      
+	    // make links link to URL
+	    // http://css-tricks.com/snippets/php/find-urls-in-text-make-links/
+	    if(preg_match($reg_exUrl, $tweet_text, $url)) {
+	 
+	       // make the urls hyper links
+	       $tweet_text = preg_replace($reg_exUrl, "<a href='{$url[0]}'>{$url[0]}</a> ", $tweet_text);
+	 
+	    }
+	 
+	    if(preg_match($reg_exHash, $tweet_text, $hash)) {
+	 
+	       // make the hash tags hyper links    https://twitter.com/search?q=%23truth
+	       $tweet_text = preg_replace($reg_exHash, "<a href='https://twitter.com/search?q={$hash[0]}'>{$hash[0]}</a> ", $tweet_text);
+	        
+	       // swap out the # in the URL to make %23
+	       $tweet_text = str_replace("/search?q=#", "/search?q=%23", $tweet_text );
+	 
+	    }
+	 
+	    if(preg_match($reg_exUser, $tweet_text, $user)) {
+	 
+	        $tweet_text = preg_replace("/@([a-z_0-9]+)/i", "<a href='http://twitter.com/$1'>$0</a>", $tweet_text);
+ 		
+ 		}
+
+ 		$tweet->text_html = $tweet_text;
+ 	}
+
+	// $tweet_feed['tewt_html'] = $tweet_feed;
+	echo json_encode($tweet_feed);
 ?>
